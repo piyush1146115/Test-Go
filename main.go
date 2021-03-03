4a9a3cb003f9adfcd6ec705c8e7116de7e1730c9
piyush1146115 <piyush@appscode.com>
2021-03-03 18:31:31 +0600

    C2: Used different data types in Go

--- main.go
+++ main.go
@@ -4,4 +4,15 @@ import "fmt"
 
 func main()  {
 	fmt.Println("Hello Go!")
+	fmt.Println("1 + 1 =", 1 + 1)
+	fmt.Println("1 + 1 =", 1.0 + 1.0)
+	fmt.Println(len("Hello World"))
+	fmt.Println("Hello World"[1])
+	fmt.Println("Hello " + "World")
+	fmt.Println(true && true)
+	fmt.Println(true && false)
+	fmt.Println(true || true)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+
 }
