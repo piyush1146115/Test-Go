a49305a4104ee1b6c2911b7f6fdcc216fc241995
piyush1146115 <piyush123kantidas@gmail.com>
2021-04-25 13:30:21 +0600

    C2: Experienced different aspects of Go channels

--- goChannelRevise.go
+++ goChannelRevise.go
@@ -1,4 +1,12 @@
 package main
+
+//import (
+//	"context"
+//	"fmt"
+//	"math/rand"
+//	"time"
+//)
+
 //
 //import "fmt"
 //
@@ -28,33 +36,153 @@ package main
 //}
 
 
+//import (
+//	"fmt"
+//	"time"
+//)
+//
+//var cnt int
+//func WaitMany(a, b chan bool) {
+//	for a != nil || b != nil {
+//		select {
+//		case <-a:
+//			a = nil
+//		case <-b:
+//			b = nil
+//		}
+//		cnt++
+//		fmt.Println("From WaitMany ", cnt)
+//	}
+//}
+//
+//func main() {
+//	a, b := make(chan bool), make(chan bool)
+//	t0 := time.Now()
+//	cnt = 0
+//	go func() {
+//		close(a)
+//		close(b)
+//	}()
+//	WaitMany(a, b)
+//	fmt.Printf("waited %v for WaitMany\n", time.Since(t0))
+//}
+
+
+
+
+
+//Fanout Pattern example
+//import (
+//	"fmt"
+//	"math/rand"
+//	"time"
+//)
+//
+//func main() {
+//	emps := 20
+//	ch := make(chan string, emps)
+//
+//	for e := 0; e < emps; e++ {
+//		go func() {
+//			time.Sleep(time.Duration(rand.Intn(200)) * time.Millisecond)
+//			ch <- "paper"
+//		}()
+//	}
+//
+//	for emps > 0 {
+//		p := <-ch
+//		fmt.Println(p)
+//		emps--
+//	}
+//}
+//
+
+
+
+
+
+
+//Drop pattern
+//import (
+//	"fmt"
+//)
+//
+//func main() {
+//	const cap = 5
+//	ch := make(chan string, cap)
+//
+//	go func() {
+//		for p := range ch {
+//			fmt.Println("employee : received :", p)
+//		}
+//	}()
+//
+//	const work = 20
+//	for w := 0; w < work; w++ {
+//		select {
+//		case ch <- "paper":
+//			fmt.Println("manager : send ack")
+//		default:
+//			fmt.Println("manager : drop")
+//		}
+//	}
+//
+//	close(ch)
+//}
+
+
+
+
+//Signal With Data - Delayed Guarantee - Buffered Channel 1
+//import (
+//	"fmt"
+//)
+//
+//func main() {
+//	ch := make(chan string, 1)
+//
+//	go func() {
+//		for p := range ch {
+//			fmt.Println("employee : working :", p)
+//		}
+//	}()
+//
+//	const work = 10
+//	for w := 0; w < work; w++ {
+//		fmt.Println("Manager : Sending :")
+//		ch <- "paper"
+//	}
+//	close(ch)
+//}
+
+
+
+//Signal Without Data - Context
 import (
-	"fmt"
-	"time"
+"context"
+"fmt"
+"math/rand"
+"time"
 )
 
-var cnt int
-func WaitMany(a, b chan bool) {
-	for a != nil || b != nil {
-		select {
-		case <-a:
-			a = nil
-		case <-b:
-			b = nil
-		}
-		cnt++
-		fmt.Println("From WaitMany ", cnt)
-	}
-}
-
 func main() {
-	a, b := make(chan bool), make(chan bool)
-	t0 := time.Now()
-	cnt = 0
+	duration := 50 * time.Millisecond
+
+	ctx, cancel := context.WithTimeout(context.Background(), duration)
+	defer cancel()
+
+	ch := make(chan string, 1)
+
 	go func() {
-		close(a)
-		close(b)
+		time.Sleep(time.Duration(rand.Intn(100)) * time.Millisecond)
+		ch <- "paper"
 	}()
-	WaitMany(a, b)
-	fmt.Printf("waited %v for WaitMany\n", time.Since(t0))
+
+	select {
+	case p := <-ch:
+		fmt.Println("work complete", p)
+
+	case <-ctx.Done():
+		fmt.Println("moving on")
+	}
 }
