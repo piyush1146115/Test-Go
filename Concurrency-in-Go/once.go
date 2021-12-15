922a645b3ac1e423ed0287266f112885f75db738
piyush1146115 <piyush@appscode.com>
2021-12-15 19:12:10 +0600

    Add once example

---
+++ Concurrency-in-Go/once.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	var count int
+	increment := func() { count++ }
+	decrement := func() { count-- }
+	var once sync.Once
+	once.Do(increment)
+	once.Do(decrement)
+	fmt.Printf("Count: %d\n", count)
+}
+
+//
+//Is it surprising that the output displays 1 and not 0 ? This is because
+//sync.Once only counts the number of times Do is called, not how many times
+//unique functions passed into Do are called
