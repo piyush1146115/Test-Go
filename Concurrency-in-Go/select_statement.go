d150a4fc14beaede8158f9c5511f4b6f37a5b242
piyush1146115 <piyush@appscode.com>
2022-01-21 12:00:08 +0600

    Practice select statement.
    Signed-off-by: piyush1146115 <piyush@appscode.com>

---
+++ Concurrency-in-Go/select_statement.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	start := time.Now()
+	c := make(chan interface{})
+
+	go func() {
+		time.Sleep(5 * time.Second)
+		close(c)
+	}()
+
+	fmt.Println("Blocking on read")
+
+	select {
+	case <-c:
+		fmt.Printf("Unblocked %v later", time.Since(start))
+	}
+}
